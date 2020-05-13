a44380ce990786da3c1ae3f790671eff2d61a5d6
Andre Anjos <andre.anjos@idiap.ch>
2020-05-13 18:22:33 +0200

    [doc] Reset scripts for baseline and xtest [ci skip]

--- doc/scripts/baselines.sh
+++ doc/scripts/baselines.sh
@@ -3,8 +3,8 @@
 # Runs all of our baselines
 
 # set output directory and location of "bob" executable
-OUTDIR=/path/to/output/diretory
-BOB=/path/to/bob/execuble
+OUTDIR=/idiap/temp/aanjos/binseg/baselines-with-validation
+BOB=/idiap/user/aanjos/work/bob/bob.ip.binseg/bin/bob
 
 # run <modelconfig> <dbconfig> <batchsize> [<device> [<queue>]]
 function run() {
@@ -15,8 +15,7 @@ function run() {
     cmd+=("-vv" "--device=${device}" ${1} ${2})
     cmd+=("--batch-size=${3}" "--output-folder=${OUTDIR}/${1}/${2}")
 
-    # notice this assumes gridtk is installed
-    [ $# -gt 4 ] && cmd=(jman submit "--memory=24G" "--queue=${5}" -- "${cmd[@]}")
+    [ $# -gt 4 ] && cmd=(jman submit "--name=$(basename ${OUTDIR})-${1}-${2}" "--memory=24G" "--queue=${5}" -- "${cmd[@]}")
 
     "${cmd[@]}"
 }
@@ -24,23 +23,23 @@ function run() {
 # run/submit all baselines
 # comment out from "sgpu/gpu" to run locally
 # comment out from "cuda:0" to run on CPU
-run m2unet stare          6 #cuda:0 #sgpu
-run hed    stare          4 #cuda:0 #sgpu
-run driu   stare          5 #cuda:0 #sgpu
-run unet   stare          2 #cuda:0 #sgpu
-run m2unet drive         16 #cuda:0 #sgpu
-run hed    drive          8 #cuda:0 #sgpu
-run driu   drive          8 #cuda:0 #sgpu
-run unet   drive          4 #cuda:0 #sgpu
-run m2unet iostar-vessel  6 #cuda:0 #sgpu
-run hed    iostar-vessel  4 #cuda:0 #sgpu
-run driu   iostar-vessel  4 #cuda:0 #sgpu
-run unet   iostar-vessel  2 #cuda:0 #sgpu
-run m2unet chasedb1       6 #cuda:0 #sgpu
-run hed    chasedb1       4 #cuda:0 #sgpu
-run driu   chasedb1       4 #cuda:0 #sgpu
-run unet   chasedb1       2 #cuda:0 #sgpu
-run m2unet hrf            1 #cuda:0 #gpu
-run hed    hrf            1 #cuda:0 #gpu
-run driu   hrf            1 #cuda:0 #gpu
-run unet   hrf            1 #cuda:0 #gpu
+run m2unet drive 16 cuda:0 sgpu
+run hed drive 8 cuda:0 sgpu
+run driu drive 8 cuda:0 sgpu
+run unet drive 4 cuda:0 sgpu
+run m2unet stare 6 cuda:0 sgpu
+run hed stare 4 cuda:0 sgpu
+run driu stare 5 cuda:0 sgpu
+run unet stare 2 cuda:0 sgpu
+run m2unet chasedb1 6 cuda:0 sgpu
+run hed chasedb1 4 cuda:0 sgpu
+run driu chasedb1 4 cuda:0 sgpu
+run unet chasedb1 2 cuda:0 sgpu
+run m2unet hrf 1 cuda:0 gpu
+run hed hrf 1 cuda:0 gpu
+run driu hrf 1 cuda:0 gpu
+run unet hrf 1 cuda:0 gpu
+run m2unet iostar-vessel 6 cuda:0 sgpu
+run hed iostar-vessel 4 cuda:0 sgpu
+run driu iostar-vessel 4 cuda:0 sgpu
+run unet iostar-vessel 2 cuda:0 sgpu
--- doc/scripts/mtest.sh
+++ doc/scripts/mtest.sh
@@ -6,9 +6,9 @@ BOB=$HOME/work/bob/bob.ip.binseg/bin/bob
 
 for d in drive stare chasedb1 iostar-vessel hrf; do
     for m in driu hed m2unet unet; do
-        cmd=(${BOB} binseg analyze -vv ${m} "${d}-xtest")
+        cmd=(${BOB} binseg analyze -vv ${m} "${d}-mtest")
         cmd+=("--weight=${m}/${d}/model/model_lowest_valid_loss.pth")
-        cmd+=("--output-folder=${m}/${d}/xtest")
+        cmd+=("--output-folder=${m}/${d}/mtest")
         "${cmd[@]}"
     done
 done
