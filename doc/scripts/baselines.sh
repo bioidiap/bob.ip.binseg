d5240bcfa7bd6efaeebd7f25a31669f3119e1a60
Andre Anjos <andre.anjos@idiap.ch>
2020-05-15 15:27:13 +0200

    [doc/scripts/baselines] Fix script

--- doc/scripts/baselines.sh
+++ doc/scripts/baselines.sh
@@ -23,23 +23,23 @@ function run() {
 # run/submit all baselines
 # comment out from "sgpu/gpu" to run locally
 # comment out from "cuda:0" to run on CPU
-run m2unet drive         16 cuda:0 sgpu
-run hed    drive          8 cuda:0 sgpu
-run driu   drive          8 cuda:0 sgpu
-run unet   drive          4 cuda:0 sgpu
-run m2unet stare          6 cuda:0 sgpu
-run hed    stare          4 cuda:0 sgpu
-run driu   stare          5 cuda:0 sgpu
-run unet   stare          2 cuda:0 sgpu
-run m2unet chasedb1       6 cuda:0 sgpu
-run hed    chasedb1       4 cuda:0 sgpu
-run driu   chasedb1       4 cuda:0 sgpu
-run unet   chasedb1       2 cuda:0 sgpu
-run m2unet hrf            1 cuda:0  gpu
-run hed    hrf            1 cuda:0  gpu
-run driu   hrf            1 cuda:0  gpu
-run unet   hrf            1 cuda:0  gpu
-run m2unet iostar-vessel  6 cuda:0 sgpu
-run hed    iostar-vessel  4 cuda:0 sgpu
-run driu   iostar-vessel  4 cuda:0 sgpu
-run unet   iostar-vessel  2 cuda:0 sgpu
+run m2unet drive         16 #cuda:0 #sgpu
+run hed    drive          8 #cuda:0 #sgpu
+run driu   drive          8 #cuda:0 #sgpu
+run unet   drive          4 #cuda:0 #sgpu
+run m2unet stare          6 #cuda:0 #sgpu
+run hed    stare          4 #cuda:0 #sgpu
+run driu   stare          5 #cuda:0 #sgpu
+run unet   stare          2 #cuda:0 #sgpu
+run m2unet chasedb1       6 #cuda:0 #sgpu
+run hed    chasedb1       4 #cuda:0 #sgpu
+run driu   chasedb1       4 #cuda:0 #sgpu
+run unet   chasedb1       2 #cuda:0 #sgpu
+run m2unet hrf            1 #cuda:0 # gpu
+run hed    hrf            1 #cuda:0 # gpu
+run driu   hrf            1 #cuda:0 # gpu
+run unet   hrf            1 #cuda:0 # gpu
+run m2unet iostar-vessel  6 #cuda:0 # gpu
+run hed    iostar-vessel  4 #cuda:0 # gpu
+run driu   iostar-vessel  4 #cuda:0 # gpu
+run unet   iostar-vessel  2 #cuda:0 # gpu
