b334cf4e3b46ad3aaf9a13bb4fc0e47c792cd3a1
Andre Anjos <andre.dos.anjos@gmail.com>
2020-09-10 08:16:12 +0200

    [configs.models.lwnet] Add lwnet cosine-anneeling training

---
+++ bob/ip/binseg/configs/models/lwnet.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Little W-Net for image segmentation
+
+The Little W-Net architecture contains roughly around 70k parameters and
+closely matches (or outperforms) other more complex techniques.
+
+Reference: [GALDRAN-2020]_
+"""
+
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim import Adam
+from torch.nn import BCEWithLogitsLoss
+from bob.ip.binseg.models.lwnet import lwnet
+
+##### Config #####
+max_lr = 0.01  #start
+min_lr = 1e-08  #valley
+cycle = 50  #epochs for a complete scheduling cycle
+
+model = lwnet()
+
+criterion = BCEWithLogitsLoss()
+
+optimizer = Adam(
+    model.parameters(),
+    lr=max_lr,
+)
+
+scheduler = CosineAnnealingLR(
+    optimizer,
+    T_max=cycle,
+    eta_min=min_lr,
+)
--- doc/scripts/baselines.sh
+++ doc/scripts/baselines.sh
@@ -35,19 +35,24 @@ run m2unet drive         16 #cuda:0 #sgpu
 run hed    drive          8 #cuda:0 #sgpu
 run driu   drive          8 #cuda:0 #sgpu
 run unet   drive          4 #cuda:0 #sgpu
+run lwnet  drive          4 #cuda:0 #sgpu
 run m2unet stare          6 #cuda:0 #sgpu
 run hed    stare          4 #cuda:0 #sgpu
 run driu   stare          5 #cuda:0 #sgpu
 run unet   stare          2 #cuda:0 #sgpu
+run lwnet  stare          4 #cuda:0 #sgpu
 run m2unet chasedb1       6 #cuda:0 #sgpu
 run hed    chasedb1       4 #cuda:0 #sgpu
 run driu   chasedb1       4 #cuda:0 #sgpu
 run unet   chasedb1       2 #cuda:0 #sgpu
+run lwnet  chasedb1       4 #cuda:0 #sgpu
 run m2unet hrf            1 #cuda:0 # gpu
 run hed    hrf            1 #cuda:0 # gpu
 run driu   hrf            1 #cuda:0 # gpu
 run unet   hrf            1 #cuda:0 # gpu
+run lwnet  hrf            4 #cuda:0 # gpu
 run m2unet iostar-vessel  6 #cuda:0 # gpu
 run hed    iostar-vessel  4 #cuda:0 # gpu
 run driu   iostar-vessel  4 #cuda:0 # gpu
 run unet   iostar-vessel  2 #cuda:0 # gpu
+run lwnet  iostar-vessel  4 #cuda:0 # gpu
