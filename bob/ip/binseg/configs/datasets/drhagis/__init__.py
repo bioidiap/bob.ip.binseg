da68fd09e9d042f3c3ac118ef6b1b5f24178e443
Driss KHALIL <driss.khalil@idiap.ch>
2021-06-21 12:36:13 +0200

    Update resizing

--- bob/ip/binseg/configs/datasets/drhagis/__init__.py
+++ bob/ip/binseg/configs/datasets/drhagis/__init__.py
@@ -3,8 +3,8 @@
 
 def _maker(protocol):
 
-    from ....data.transforms import Resize,  CenterCrop as ccrop
+    from ....data.transforms import Resize
     from ....data.drhagis import dataset as raw
     from .. import make_dataset as mk
-    return mk(raw.subsets(protocol), [Resize((1880,1880))])
+    return mk(raw.subsets(protocol), [Resize((1760,1760))])
 
