259bf5b150a8ab43b65dc5cec2eff921639aaef3
Matheus Renzo <m.renzo@murabei.com>
2022-09-27 13:31:18 -0300

    adding configs for detection datasets

---
+++ bob/ip/detect/configs/datasets/montgomery/default.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""Montgomery County dataset for Lung Detection (default protocol)
+
+* Split reference: [GAAL-2020]_
+* Configuration resolution: 256 x 256
+* See :py:mod:`bob.ip.detect.data.montgomery` for dataset details
+"""
+
+from bob.ip.detect.configs.datasets.montgomery import _maker_augmented
+
+dataset = _maker_augmented("default")
