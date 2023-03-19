d034d80236afce42688a6efc45c9e4c4c93b6c13
Txiao <xiao.tan@uzh.ch>
2023-03-19 13:39:15 +0100

    test0

--- src/deepdraw/binseg/configs/datasets/hrf/semi_768.py
+++ src/deepdraw/binseg/configs/datasets/hrf/semi_768.py
@@ -11,17 +11,24 @@ from ..hrf.default_768 import dataset as _hrf
 
 jitter = T.ColorJitter(saturation=0.3, brightness=0.3, contrast=0.3)
 gray = T.Grayscale(num_output_channels=3)
-train = []
-for i in _hrf["train"]:
-    j = i
-    j[1] = i[1] + 0.01 * torch.randn_like(i[1])  # add gaussian noise
-    j[1] = gray(j[1])
-    # j[1] = jitter(j[1])
-    train.append(j)
+
+
+def _transform(dataset):
+    train = []
+    for i in dataset:
+        j = i
+        j[1] = i[1] + 0.01 * torch.randn_like(i[1])  # add gaussian noise
+        j[1] = gray(j[1])
+        # j[1] = jitter(j[1])
+        # j[1] = rotate(j[1])
+        train.append(j)
+    return train
+
+
 dataset = {
     "train": _hrf["train"],
     "test": _hrf["test"],
-    "__train__": train,
+    "__train__": _transform(_hrf["train"]),
     "__valid__": _hrf["train"],
 }
 dataset["__extra_valid__"] = [dataset["test"]]
--- src/deepdraw/binseg/configs/datasets/stare/semi_768.py
+++ src/deepdraw/binseg/configs/datasets/stare/semi_768.py
@@ -12,18 +12,23 @@ from ..stare.ah_768 import dataset as _stare
 jitter = T.ColorJitter(saturation=0.3)
 gray = T.Grayscale(num_output_channels=3)
 
-train = []
-for i in _stare["train"]:
-    j = i
-    # j[1] = T.functional.adjust_sharpness(j[1],sharpness_factor=0.0) # sharpness
-    j[1] = i[1] + 0.01 * torch.randn_like(i[1])  # add gaussian noise
-    j[1] = gray(j[1])
-    # j[1] = jitter(j[1])
-    train.append(j)
+
+def _transform(dataset):
+    train = []
+    for i in dataset:
+        j = i
+        j[1] = i[1] + 0.01 * torch.randn_like(i[1])  # add gaussian noise
+        j[1] = gray(j[1])
+        # j[1] = jitter(j[1])
+        # j[1] = rotate(j[1])
+        train.append(j)
+    return train
+
+
 dataset = {
-    "train": train,
+    "train": _stare["train"],
     "test": _stare["test"],
-    "__train__": train,
+    "__train__": _transform(_stare["train"]),
     "__valid__": _stare["train"],
 }
 dataset["__extra_valid__"] = [dataset["test"]]
