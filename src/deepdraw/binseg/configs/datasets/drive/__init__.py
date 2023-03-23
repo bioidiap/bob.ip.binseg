7036d1221f1b3f1a667f2db7231c2aeaef6eb515
Txiao <xiao.tan@uzh.ch>
2023-03-23 22:22:42 +0100

    change data augmentation class position

--- src/deepdraw/binseg/configs/datasets/drive/__init__.py
+++ src/deepdraw/binseg/configs/datasets/drive/__init__.py
@@ -1,11 +1,13 @@
 # SPDX-FileCopyrightText: Copyright © 2023 Idiap Research Institute <contact@idiap.ch>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-#import torch
+# import torch
 import torchvision.transforms as T
 
 gray = T.Grayscale(num_output_channels=3)
 jitter = T.ColorJitter(hue=0.05)
+
+
 def _maker(protocol):
     from .....common.data.transforms import CenterCrop as ccrop
     from ....data.drive import dataset as raw
@@ -25,10 +27,18 @@ def _maker_square(protocol, size):
 
 
 def _semi_data_augmentation(protocol, size):
+    from .....common.data.transforms import Gaussian_noise as noise
+    from .....common.data.transforms import Pad, Resize
     from ....data.drive import dataset as raw
     from .. import make_dataset as mk
-    from .....common.data.transforms import (
-        Gaussian_noise as noise, Pad, Resize
-    )
 
-    return mk(raw.subsets(protocol), [Pad((10, 1, 10, 0)), Resize((size, size)),jitter,gray, noise(0, 0.01)])
+    return mk(
+        raw.subsets(protocol),
+        [
+            Pad((10, 1, 10, 0)),
+            Resize((size, size)),
+            jitter,
+            gray,
+            noise(0, 0.01),
+        ],
+    )
