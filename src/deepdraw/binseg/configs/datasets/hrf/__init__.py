634d732f4295eae49df17007999ae603f3695186
Txiao <xiao.tan@uzh.ch>
2023-03-24 19:32:49 +0100

    fix data augmentation

--- src/deepdraw/binseg/configs/datasets/hrf/__init__.py
+++ src/deepdraw/binseg/configs/datasets/hrf/__init__.py
@@ -48,18 +48,46 @@ def _maker_square(protocol, size):
 
 
 def _semi_data_augmentation(protocol, size):
+    from .....common.data.transforms import Compose
     from .....common.data.transforms import Gaussian_noise as noise
     from .....common.data.transforms import Pad, Resize
     from ....data.hrf import dataset as raw
-    from .. import make_dataset as mk
+    from .. import make_subset
 
-    return mk(
-        raw.subsets(protocol),
-        [
-            Pad((0, 584)),
-            Resize((size, size)),
-            jitter,
-            gray,
-            noise(0, 0.01),
+    # resize all data
+    # add gaussian noise, color jitter and grayscale to the training data
+
+    def make_aug_subsets(subsets, train_transforms, all_transforms):
+        retval = {}
+
+        for key in subsets.keys():
+            retval[key] = make_subset(subsets[key], transforms=all_transforms)
+            if key == "train":
+                retval["__train__"] = make_subset(
+                    subsets[key],
+                    transforms=train_transforms,
+                )
+            else:
+                if key == "validation":
+                    retval["__valid__"] = retval[key]
+
+        if ("__train__" in retval) and ("__valid__" not in retval):
+            retval["__valid__"] = retval["__train__"]
+
+        return retval
+
+    return make_aug_subsets(
+        subsets=raw.subsets(protocol),
+        all_transforms=[Pad((0, 584)), Resize((size, size))],
+        train_transforms=[
+            Compose(
+                [
+                    Pad((0, 584)),
+                    Resize((size, size)),
+                    jitter,
+                    gray,
+                    noise(0, 0.01),
+                ]
+            )
         ],
     )
